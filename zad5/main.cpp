36b121f0e99a72e76aa67ec3cb3fd2336d94c3d2
Michał Folwarski <folwarski.michal@gmial.com>
2018-05-03 00:55:48 +0200

    zad5 refactoring

--- zad5/main.cpp
+++ zad5/main.cpp
@@ -15,9 +15,11 @@ struct myNumber {
 };
 
 bool isPrime(long number);
+void manageProcesses(uint numberOfThreads, vector<myNumber> &numbers);
 void printResults(vector<myNumber> numbers);
-void update(vector<myNumber> &numbers, myNumber newNumber);
+void processData();
 vector<myNumber> readNumbers(ifstream &inputFile);
+void update(vector<myNumber> &numbers, myNumber newNumber);
 
 int main(int argc, char **argv) {
     Init(argc, argv);
@@ -35,62 +37,32 @@ int main(int argc, char **argv) {
     }
 
     uint numberOfThreads = COMM_WORLD.Get_size();
-    int worldRank = COMM_WORLD.Get_rank();
+    int rank = COMM_WORLD.Get_rank();
 
     vector <myNumber> numbers;
-    myNumber currentNumber;
-    uint i;
+    double startTime;
 
     COMM_WORLD.Barrier();
-    double startTime = Wtime();
 
-    if (worldRank == 0) { //MASTER
+    if (rank == 0) {
         numbers = readNumbers(inputFile);
         inputFile.close();
+        startTime = Wtime();
 
         if (numberOfThreads == 1) {
-            for (i=0; i < numbers.size(); ++i) {
+            for (uint i=0; i < numbers.size(); ++i) {
                 numbers[i].prime = isPrime(numbers[i].value);
             }
         } else {
-            myNumber numberToTest;
-            Status status;
-            uint counter = 0;
-
-            for (uint i = 1; i < numberOfThreads; ++i) {
-                if ( (i-1) < numbers.size()) {
-                    numberToTest = numbers[counter++];
-                } else {
-                    numberToTest.value = STOP_SIGNAL;
-                }
-                COMM_WORLD.Send(&numberToTest, sizeof(struct myNumber), CHAR, i, 0);
-            }
-
-            for (uint i = 0; i < numbers.size(); ++i) {
-                COMM_WORLD.Recv(&currentNumber, sizeof(struct myNumber), CHAR, ANY_SOURCE, 0, status);
-                update(numbers, currentNumber);
-                if (counter < numbers.size()) {
-                    numberToTest = numbers[counter++];
-                } else {
-                    numberToTest.value = STOP_SIGNAL;
-                }
-                COMM_WORLD.Send(&numberToTest, sizeof(struct myNumber), CHAR, status.Get_source(), 0);
-            }
+            manageProcesses(numberOfThreads, numbers);
         }
-    } else { //SLAVE
-        while(true) {
-            COMM_WORLD.Recv(&currentNumber, sizeof(struct myNumber), CHAR, 0, 0);
-            if (currentNumber.value == STOP_SIGNAL) {
-                break;
-            }
-            currentNumber.prime = isPrime(currentNumber.value);
-            COMM_WORLD.Send(&currentNumber, sizeof(struct myNumber), CHAR, 0, 0);
-        }
+    } else { //rank != 0
+        processData();
     }
 
     COMM_WORLD.Barrier();
-    double endTime = Wtime();
-    if (worldRank == 0) {
+    if (rank == 0) {
+        double endTime = Wtime();
         cout << "Time: " << (endTime - startTime) * 1000 << "ms" << endl;
         printResults(numbers);
     }
@@ -157,11 +129,65 @@ void printResults(vector<myNumber> numbers) {
     }
 }
 
+
+/*
+ * Updates the list of numbers.
+ *
+ * @param numbers list of numbers
+ * @param newNumber number to update
+ */
 void update(vector<myNumber> &numbers, myNumber newNumber) {
     for (uint i = 0; i < numbers.size(); ++i) {
         if (numbers[i].value == newNumber.value) {
             numbers[i].prime = newNumber.prime;
             return;
         }
+    }
+}
+
+/**
+ * Manages processes - sends and receives data.
+ *
+ * @param numberOfThreads number of threads
+ * @param numbers list of numbers
+ */
+void manageProcesses(uint numberOfThreads, vector<myNumber> &numbers) {
+    myNumber numberToTest, receivedNumber;
+    Status status;
+    uint counter = 0;
+
+    for (uint i = 1; i < numberOfThreads; ++i) {
+        if ( (i-1) < numbers.size()) {
+            numberToTest = numbers[counter++];
+        } else {
+            numberToTest.value = STOP_SIGNAL;
+        }
+        COMM_WORLD.Send(&numberToTest, sizeof(struct myNumber), CHAR, i, 0);
+    }
+
+    for (uint i = 0; i < numbers.size(); ++i) {
+        COMM_WORLD.Recv(&receivedNumber, sizeof(struct myNumber), CHAR, ANY_SOURCE, 0, status);
+        update(numbers, receivedNumber);
+        if (counter < numbers.size()) {
+            numberToTest = numbers[counter++];
+        } else {
+            numberToTest.value = STOP_SIGNAL;
+        }
+        COMM_WORLD.Send(&numberToTest, sizeof(struct myNumber), CHAR, status.Get_source(), 0);
+    }
+}
+
+/**
+ * Receives number, checks primality and sends the result back.
+ */
+void processData() {
+    myNumber receivedNumber;
+    while(true) {
+        COMM_WORLD.Recv(&receivedNumber, sizeof(struct myNumber), CHAR, 0, 0);
+        if (receivedNumber.value == STOP_SIGNAL) {
+            break;
+        }
+        receivedNumber.prime = isPrime(receivedNumber.value);
+        COMM_WORLD.Send(&receivedNumber, sizeof(struct myNumber), CHAR, 0, 0);
     }
 }
