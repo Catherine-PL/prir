94c99264607209f4ba348668d6c98e4a90682f8c
Catherine <katarzyna.kosiak@o2.pl>
2018-05-17 23:39:11 +0200

    zad3 init

---
+++ zad3/main.cu
@@ -0,0 +1,10 @@
+#include "cuda.h"
+
+//TODO add arguments: input, output, arraySum
+__global__ void filter() {
+	//outputImage.at<cv::Vec3b>(i, j)[0] = getGauss(inputImage, 0, i, j, arraySum);
+	//outputImage.at<cv::Vec3b>(i, j)[1] = getGauss(inputImage, 1, i, j, arraySum);
+	//outputImage.at<cv::Vec3b>(i, j)[2] = getGauss(inputImage, 2, i, j, arraySum);
+	//TODO
+
+}
