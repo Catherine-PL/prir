786abd7b63c217a5528c0eb720375f54599219e9
KK <kdrukowanie@gmail.com>
2018-05-22 20:44:28 +0200

    zad3 setup cuda

--- zad3/main.cu
+++ zad3/main.cu
@@ -15,15 +15,6 @@ static int HandleError(cudaError_t err, const char *file, int line)
 	return 0;
 }
 
-//TODO add arguments: input, output, arraySum
-__global__ void filter() {
-	//outputImage.at<cv::Vec3b>(i, j)[0] = getGauss(inputImage, 0, i, j, arraySum);
-	//outputImage.at<cv::Vec3b>(i, j)[1] = getGauss(inputImage, 1, i, j, arraySum);
-	//outputImage.at<cv::Vec3b>(i, j)[2] = getGauss(inputImage, 2, i, j, arraySum);
-	//TODO
-
-}
-
 int GAUSS[25] = {1, 1, 2, 1, 1,
                  1, 2, 4, 2, 1,
                  2, 4, 8, 4, 2,
@@ -31,58 +22,91 @@ int GAUSS[25] = {1, 1, 2, 1, 1,
                  1, 1, 2, 1, 1
                 };
 
+__constant__ int gaussMask[25];
+__constant__ int gaussSum;
+
+//TODO add arguments: input, output, arraySum
+__global__ void filter(unsigned char* inputImage, unsigned char* outputImage) {
+	//TODO i and j
+	//outputImage.at<cv::Vec3b>(i, j)[0] = getGauss(inputImage, 0, i, j, arraySum);
+	//outputImage.at<cv::Vec3b>(i, j)[1] = getGauss(inputImage, 1, i, j, arraySum);
+	//outputImage.at<cv::Vec3b>(i, j)[2] = getGauss(inputImage, 2, i, j, arraySum);
+	//TODO
+
+}
+
 //TODO this will run on each thread in each block
-int getGauss(Mat image, int channel, int i, int j, int arraySum) {
+/*__device__ int getGauss(Mat image, int channel, int i, int j, int arraySum) {
     int value = 0;
     int outOfBounds = 0;
     for (int x = -2; x <= 2; x++) {
         for (int y = -2; y <=2; y++) {
             if (i + x < 0 || i + x >= image.rows || j + y < 0 || j + y > image.cols ) {
-                outOfBounds += GAUSS[5 * (2 + x) + 2 + y];
+                outOfBounds += gaussMask[5 * (2 + x) + 2 + y];
             } else {
-                value += image.at<Vec3b>(i + x, j + y)[channel]*GAUSS[5 * (2 + x) + 2 + y];
+                value += image.at<Vec3b>(i + x, j + y)[channel]*gaussMask[5 * (2 + x) + 2 + y];
             }
         }
     }
     return value / (arraySum - outOfBounds);
-}
+}*/
 
 int main(int argc, char **argv) {
-    if (argc != 2 ) {
-        std::cout << "Invalid number of arguments!" << std::endl;
+    if (argc != 3 ) {
+        std::cout << "Invalid number of arguments!" << argc << std::endl;
         return EXIT_FAILURE;
     }
-    Mat inputImage = imread(argv[0], IMREAD_COLOR);
+    Mat inputImage = imread(argv[1], IMREAD_COLOR);
     if (inputImage.empty() ) {
-        std::cout << "File doesn't exist: " << argv[0] << std::endl;
+        std::cout << "File doesn't exist: " << argv[1] << std::endl;
         return EXIT_FAILURE;
     }
-    int numberOfThreads = atoi(argv[1]);
     Mat outputImage = inputImage.clone();
 
-    // get gauss weights sum
-    int arraySum = 0;
-    for(int count = 0; count < 25; count++) {
-        arraySum += GAUSS[count];
-    }
+    //GAUSS ARRAY SUM
+    int *arraySum;
+        for(int count = 0; count < 25; count++) {
+            arraySum += GAUSS[count];
+        }
+/*
+ * commented because I use constant gaussSum instead
+    int *devArraySum = NULL;
+    HANDLE_ERROR(cudaMalloc((void**) &devArraySum, sizeof(unsigned int)));
+    HANDLE_ERROR(cudaMemcpy(devArraySum, arraySum, sizeof(unsigned int), cudaMemcpyHostToDevice));
+*/
 
-	//TODO
-	// assing memory in CUDA for the input image, output image, arraySum
-	// call the CUDA method
+    // GAUSS MASK SIZE
+    HANDLE_ERROR(cudaMemcpyToSymbol(gaussSum, &arraySum, sizeof(int)));
 
+    //GAUSS MASK
+    HANDLE_ERROR(cudaMemcpyToSymbol(gaussMask, GAUSS, sizeof(int) * 25));
+
+    //INPUT AND OUTPUT IMAGES
+    unsigned char *devInputImage, *devOutputImage;
+    int imageMemSize = inputImage.rows*inputImage.step;
+    HANDLE_ERROR(cudaMalloc<unsigned char>(&devInputImage, imageMemSize));
+    HANDLE_ERROR(cudaMalloc<unsigned char>(&devOutputImage, imageMemSize));
+
+    //step – Number of bytes each matrix row occupies. The value includes the padding bytes at the end of each row, if any
+    HANDLE_ERROR(cudaMemcpy(devInputImage, inputImage.ptr(), imageMemSize, cudaMemcpyHostToDevice));
+
+
+    //GRIDS AND THREADS SIZES
 	int blockSide = 128;
 	int gridX = (inputImage.rows + blockSide - 1) / blockSide;
 	int gridY = (inputImage.cols + blockSide - 1) / blockSide;
 
 	dim3 grids(gridX, gridY);
 	dim3 threads(blockSide, blockSide);
-	filter << <grids, threads >> >();
+
+	//MAIN CALL TO CUDA
+	filter <<<grids, threads >>>(devInputImage, devOutputImage);
 	// it should call _device_ method for each thread in each block
 	// copy content of output image back to C
 	//measure time
 
 
   
-    imwrite(argv[1], outputImage);
+    imwrite(argv[2], outputImage);
     return EXIT_SUCCESS;
 }
